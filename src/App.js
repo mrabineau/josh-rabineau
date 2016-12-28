310a61ecd9b7c63b39269e6e24ae23f9a1f0baa9
Mel Rabineau <melanie.rabineau1@gmail.com>
2016-12-28 15:01:00 -0800

    landing page centered

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ class App extends Component {
   render() {
     return (
       <div id="App">
-          <a id="button1"> <Link to="/main"> Enter </Link> </a>
+        <div id="appContainer">
+          <a id="button1"> <Link to="/main"> <h1>  Enter </h1> </Link> </a>
+          <img src="/images/josh-portfolio/MERON2.jpg" alt="img" id="meron" />
+        </div>
       </div>
     );
   }
