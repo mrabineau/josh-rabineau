86c591bea6c9223eacd86411792c72ec6a3cb874
Mel Rabineau <melanie.rabineau1@gmail.com>
2017-02-07 12:35:11 -0800

    react bootstrap

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// import { Breadcrumb } from 'react-bootstrap;';
 import './Projects.css'
 import House from './House.js'
 import Cells from './Cells.js'
@@ -6,26 +7,31 @@ import Collage from './Collage.js'
 import Hands from './Hands.js'
 
 
+
 class Projects extends Component {
 
   render () {
     return (
+
       <div id="projects">
+        <div>
         <div id="cellpix" className="allImages">
           <h1 className="heading" id="c"> Cells </h1>
             <Cells />
+            </div>
               <div id="housepix" className="allImages">
                 <h1 className="heading"> House </h1>
                   <House />
                </div>
+                     <div id="handspix" className="allImages">
+                        <h1 className="heading"> Hands </h1>
+                          <Hands />
+                     </div>
                       <div id="collagepix" className="allImages">
                         <h1 className="heading"> Collage </h1>
                          <Collage />
                         </div>
-                              <div id="handspix" className="allImages">
-                                 <h1 className="heading"> Hands </h1>
-                                   <Hands />
-                              </div>
+
             </div>
       </div>
       )
